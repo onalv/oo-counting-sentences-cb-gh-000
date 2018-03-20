6ee8ac389c4ddd820e16b4c745659ca218d0de3e
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-20 17:35:16 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,11 +7,11 @@ class String
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
